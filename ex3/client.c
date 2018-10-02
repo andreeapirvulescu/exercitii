ac1f0a2a413015e59891515e2fc42b1578b2e57b
Andreea Pirvulescu <apirvulescu@luxoft.com>
2018-10-02 10:20:13 +0300

    ex3

---
+++ ex3/client.c
@@ -0,0 +1,85 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdlib.h> 
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+   
+int main(int argc, char *argv[]) 
+{ 
+    struct sockaddr_in address; 
+    int client_fd, valread, port, file_size, fd; 
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0}, file_name[20];
+    char *not_found_message = "File not found";
+    int remain_data = 0, received_bytes = 0;
+
+    if (argc < 4)
+	{
+		printf("Usage: %s server_ip server_port file\n", argv[0]);
+		return 1;
+	}
+
+	port = atoi(argv[2]);
+
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        perror("Error opening socket");
+        return 1; 
+    } 
+   
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+   
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)  
+    { 
+        printf("Invalid server address\n"); 
+        return 1; 
+    }   
+   
+    if (connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("Connection Failed\n"); 
+        return 1; 
+    }
+
+    send(client_fd, argv[3], strlen(argv[3]), 0); 
+    printf("Hello message sent\n");
+
+    recv(client_fd, buffer, BUFFER_SIZE, 0);
+    if (strcmp(buffer, not_found_message) == 0)
+    {
+    	printf("There is no file with that name on the server\n");
+    } else {
+    	file_size = atoi(buffer);
+    	printf("%s\n", buffer);
+    	sprintf(file_name, "client%d", getpid());
+    	if ((fd = open(file_name, O_WRONLY | O_CREAT, 0666)) == -1)
+    	{
+    		perror("Error with opening the file");
+    	}
+    	printf("fd is %i", fd);
+    	remain_data = file_size;
+        memset(buffer, 0, sizeof(buffer));
+    	while ((received_bytes = recv(client_fd, buffer, BUFFER_SIZE, 0) > 0) && (remain_data >= 0))
+    	{	 		
+    		printf("fdddddd is %i\n", fd);
+    		write(fd, buffer, sizeof(buffer));
+    		remain_data -= received_bytes;
+    		memset(buffer, 0, sizeof(buffer));
+    	}
+    }
+
+   	close(fd);
+   	close(client_fd);
+
+    return 0; 
+}
---
+++ ex3/server.c
@@ -0,0 +1,125 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/sendfile.h>
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <fcntl.h>
+#include <string.h> 
+
+#define MAX_CLIENTS 20
+#define BUFFER_SIZE 1024 
+
+int main(int argc, char *argv[]) 
+{ 
+    int server_fd, new_socket, port, valread, fd; 
+    struct sockaddr_in address; 
+    int addrlen = sizeof(address); 
+    char buffer[BUFFER_SIZE] = {0}; 
+    char *not_found_message = "File not found";
+    struct stat file_stat;
+    char file_size[256];
+    int remain_data, bytes_read = 0;
+    off_t offset;
+
+    if (argc < 2)
+	{
+		printf("Usage: %s port \n", argv[0]);
+		return 1;
+	}
+
+	port = atoi(argv[1]);
+       
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("Error opening socket:");
+        return 1; 
+    } 
+       
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons(port); 
+       
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) 
+    { 
+        perror("ERROR in bind"); 
+        return 1; 
+    }
+
+    if (listen(server_fd, MAX_CLIENTS) < 0) 
+    { 
+        perror("Error in listen"); 
+        return 1; 
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+    		(socklen_t*)&addrlen)) < 0) 
+    { 
+        perror("Error in accept"); 
+        return 1; 
+    } 
+
+    valread = recv(new_socket, buffer, BUFFER_SIZE, 0); 
+    printf("%s\n", buffer);
+
+    if (stat(buffer, &file_stat))
+    {
+    	send(new_socket, not_found_message, strlen(not_found_message), 0); 
+    } else {
+    	if ((fd = open(buffer, O_RDONLY)) == -1)
+    	{
+    		perror("Error with opening the file");
+    	}
+    	sprintf(file_size, "%ld", file_stat.st_size);
+    	send(new_socket, file_size, strlen(file_size), 0);
+    	offset = 0;
+    	remain_data = file_stat.st_size;
+    	memset(buffer, 0, sizeof(buffer));
+    	/*while(((sent_bytes = sendfile(new_socket, fd, &offset, BUFFER_SIZE)) > 0) &&
+    			(remain_data > 0))
+    	{
+    		printf("sent bytes %i\n", sent_bytes);
+    		remain_data -= sent_bytes;
+    		memset(buffer, 0, sizeof(buffer));
+    	}*/
+    	/*while((sent_bytes = read(fd, buffer, BUFFER_SIZE)) > 0)
+    	{
+    		send(new_socket, buffer, sent_bytes, 0);
+    		memset(buffer, 0, sizeof(buffer));
+    	}*/
+    	while(1)
+    	{
+    		bytes_read =  read(fd, buffer, BUFFER_SIZE);
+    		if (bytes_read == 0)
+    		{
+    			break;
+    		}
+    		if (bytes_read < 0)
+    		{
+    			perror("Error during read");
+    		}
+    		void *p = buffer;
+    		while (bytes_read > 0)
+    		{
+    			int bytes_written = send(new_socket, p, bytes_read, 0);
+    			if (bytes_written < 0)
+    			{
+    				perror("Error during send");
+    			}
+    			bytes_read -= bytes_written;
+    			p += bytes_written;
+    			memset(buffer, 0, sizeof(buffer));
+    		}
+
+    	}
+
+    }
+    
+    close(fd);
+    close(server_fd);
+    close(new_socket);
+
+    return 0; 
+}
